9f9afcdd81e1712b22a108dc00943ef4528dedc6
John Hur <hur.john628@gmail.com>
2021-11-23 03:24:58 -0600

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,48 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type = 'input',
+        name = 'user',
+        message = 'What is your Github user name?'
+    },
+    {
+        type = 'input',
+        name = 'email',
+        message = 'What is your email?'
+    },
+    {
+        type = 'input',
+        name = 'title',
+        message = 'What is the title of your project?'
+    },
+    {
+        type = 'input',
+        name = 'description',
+        message = 'Give your project a nice description.'
+    },
+    {
+        type = 'input',
+        name = 'installation',
+        message = 'Give your project installation instructions.'
+    },
+    {
+        type = 'input',
+        name = 'usage',
+        message = 'Give your project some usage information.'
+    },
+    {
+        type = 'input',
+        name = 'contributing',
+        message = 'Give your project some contributing guidelines.'
+    },
+    {
+        type = 'input',
+        name = 'tests',
+        message = 'Give your project some test instructions'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
